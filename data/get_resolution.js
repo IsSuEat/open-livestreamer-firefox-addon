5d1cbf43472b036c058ec4841d2d7355f16ae05e
issue <armin@praher.net>
2014-11-05 17:57:32 +0100

    tweaked quality selection

--- data/get_resolution.js
+++ data/get_resolution.js
@@ -1,6 +1,6 @@
 var btns = document.getElementsByTagName("button");
 //add eventlisteners to all the found buttons, on click the buttons name is emitted to main.js
-for (var i = 0; i < btns.length; ++i){
+for (var i = 0; i < btns.length; ++i) {
     btns[i].addEventListener("click", emit, false);
 }
 function emit(e) {
