115def12c38951ffed528aff916c6766d2bd5bac
baizon <xmen7775@yahoo.de>
2015-02-17 17:37:52 +0100

    Youtube regex bugfix.

--- data/checkNode.js
+++ data/checkNode.js
@@ -22,17 +22,17 @@ self.on("context", function(node){
 	if (undefined != url) {
 		// Twitch.tv matches.
 		if(!url.match("http://www.twitch.tv/directory") && !url.match("http://www.twitch.tv/signup") && !url.match("http://www.twitch.tv/login") && 
-			url.match(/^http(s)?:\/\/(\w+\.)*twitch.tv\/[A-Za-z0-9 _]+(\/[a-z]+\/[0-9]+(\?t=([0-9]*h)?([0-9]*m)?([0-9]*s)?)?)?$/i)) {
+			url.match(/^http(s)?:\/\/(\w+\.)*twitch.tv\/[A-Za-z0-9 _-]+(\/[a-z]+\/[0-9]+(\?t=([0-9]*h)?([0-9]*m)?([0-9]*s)?)?)?$/i)) {
 			gurl = url;
 			return true; 
 		} 
 		// hitbox.tv matches.
-		else if (url.match(/^http(s)?:\/\/(\w+\.)*hitbox.tv\/[A-Za-z0-9 _]+(\/video\/[0-9]+)?$/i)) {
+		else if (url.match(/^http(s)?:\/\/(\w+\.)*hitbox.tv\/[A-Za-z0-9 _-]+(\/video\/[0-9]+)?$/i)) {
 			gurl = url;
 			return true; 
 		}
 		// youtube.com matches.
-		else if (url.match(/http(s)?:\/\/www.youtube.com\/watch\?v=[A-Za-z0-9 _]+/i)) {
+		else if (url.match(/^http(s)?:\/\/www.youtube.com\/watch\?v=[A-Za-z0-9 _-]+$/i)) {
 			gurl = url;
 			return true; 
 		} 
